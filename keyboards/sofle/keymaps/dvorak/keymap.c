47638b2e9d3f4ad73e6021ae905f3a93a536f737
Milind Kamble <milindkamble@yahoo.com>
2020-10-02 18:44:27 -0500

    more tweaks

--- keyboards/sofle/keymaps/dvorak/keymap.c
+++ keyboards/sofle/keymaps/dvorak/keymap.c
@@ -76,19 +76,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  `   |   1  |   2  |   3  |   4  |   5  |                    |  6   |  7   |  8   |  9   |  0   | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LShift|   !  |   @  |   #  |   $  |   %  |-------.    ,-------|  ^   |  &   |  *   |  (   |  )   |RShift|
+ * |LShift|   !  |   @  | LALT | LCTL |   %  |-------.    ,-------|  _   | RCTR | RALT |  {   |  }   |RShift|
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * |  {   |  { [ |  } ] |  }   |  +   |  Tab |-------|    |-------|  + = | _ -  | : ;  | \ |  |  |   |  ? / |
+ * |  {   |   [  |   ]  |  :   |  +   |  Tab |-------|    |-------|   =  |  -   |  ;   |  \   |  |   |   ?  |
  * `-----------------------------------------/      /      \      \-----------------------------------------'
  *            | LGUI | LAlt | LCTR |LOWER | / SPC  /        \ENTER \  |RAISE | RCTR | RAlt | RGUI |
  *            |      |      |      |      |/      /          \      \ |      |      |      |      |
  *            `----------------------------------'            '------''---------------------------'
  */
 [_LOWER] = LAYOUT( \
-  KC_QUES,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
-  KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,  \
-  _______,  KC_EXLM, KC_AT,   KC_LALT, KC_LCTL,  KC_PERC,                         KC_CIRC, KC_RCTL, KC_RALT, KC_LPRN, KC_RPRN, _______, \
-   KC_LCBR,  KC_LBRC,  KC_RBRC, KC_RCBR, KC_PLUS, _______, _______,       _______, KC_EQL, KC_MINS, KC_SCLN, KC_BSLS, KC_PIPE, _______, \
+  KC_QUES,  KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
+  KC_GRV,   KC_1,     KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,  \
+  _______,  KC_EXLM,  KC_AT,   KC_LALT, KC_LCTL, KC_PERC,                         KC_UNDS, KC_RCTL, KC_RALT, KC_LCBR, KC_RCBR, _______, \
+  KC_LCBR,  KC_LBRC,  KC_RBRC, KC_COLN, KC_PLUS, _______, _______,       _______, KC_EQL,  KC_MINS, KC_SCLN, KC_BSLS, KC_PIPE, KC_QUES, \
                        _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______\
 ),
 /* RAISE
