c7073bda4c5d29c5eaaad1e8789a2e4adc743041
Milind Kamble <milindkamble@gmail.com>
2020-10-07 10:05:03 -0500

    Repositioned / and ? on lower layer

--- keyboards/sofle/keymaps/dvorak/keymap.c
+++ keyboards/sofle/keymaps/dvorak/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LShift|   !  |   @  | LALT | LCTL |   +  |-------.    ,-------|  -   | RCTL | RALT |  {   |  }   |RShift|
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * | Del  |   [  |   ]  |  :   |  _   |  Tab |-------|    |-------|   =  |   ?  |  ;   |  \   |  |   |  /   |
+ * | Del  |   [  |   ]  |  :   |  _   |  Tab |-------|    |-------|   =  |   /  |  ;   |  \   |  |   |  ?   |
  * `-----------------------------------------/      /      \      \-----------------------------------------'
  *            | LGUI | LALT | LCTL |LOWER | / SPC  /        \ENTER \  |RAISE | RCTL | RALT | RGUI |
  *            |      |      |      |      |/      /          \      \ |      |      |      |      |
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F11,  KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,                            KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
   KC_GRV,   KC_1,     KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
   _______,  KC_EXLM,  KC_AT,   KC_LALT, KC_LCTL, KC_PLUS,                         KC_MINS, KC_RCTL, KC_RALT, KC_LCBR, KC_RCBR, _______, \
-  _______,  KC_LBRC,  KC_RBRC, KC_COLN, KC_UNDS, _______, _______,       _______, KC_EQL,  KC_QUES, KC_SCLN, KC_BSLS, KC_PIPE, _______, \
+  _______,  KC_LBRC,  KC_RBRC, KC_COLN, KC_UNDS, _______, _______,       _______, KC_EQL,  KC_SLSH, KC_SCLN, KC_BSLS, KC_PIPE, KC_QUES, \
                        _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______\
 ),
 /* RAISE
